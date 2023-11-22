91edfd8c464dcc5b53c8c3e653e1d941edb1dd85
isaaccheung0930 <zceewh0@ucl.ac.uk>
2023-11-22 18:38:00 +0000

    First CNN for task B

--- A/task_A_class.py
+++ A/task_A_class.py
@@ -1,5 +1,7 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 from sklearn.model_selection import GridSearchCV, KFold, cross_val_score
 
@@ -16,15 +18,15 @@ class SVM:
         val_labels = data['val_labels']
 
         # Reshaping the data to match the syntax of sklearn.
-        self.train_labels = train_labels.ravel()
-        self.test_labels = test_labels.ravel()
-        self.val_labels = val_labels.ravel()
-        self.train_images = train_images.reshape((train_images.shape[0], train_images[0].size))
-        self.test_images = test_images.reshape((test_images.shape[0], test_images[0].size))
-        self.val_images = val_images.reshape((val_images.shape[0], val_images[0].size))
+        self.__train_labels = train_labels.ravel()
+        self.__test_labels = test_labels.ravel()
+        self.__val_labels = val_labels.ravel()
+        self.__train_images = train_images.reshape((train_images.shape[0], train_images[0].size))
+        self.__test_images = test_images.reshape((test_images.shape[0], test_images[0].size))
+        self.__val_images = val_images.reshape((val_images.shape[0], val_images[0].size))
 
         # Define the kernel used for the SVM.
-        self.kernel = kernel
+        self.__kernel = kernel
 
     def train_model(self):
         self.__hyperparameters()
@@ -45,48 +47,87 @@ class SVM:
         r_range = np.logspace(-3, 3, 7)
         
         # Performing gridsearch for best parameters, kernel defaults to be linear
-        if self.kernel == 'rbf':
+        if self.__kernel == 'rbf':
             grid_parameter = {'C' : C_range, 'gamma' : gamma_range, 'random_state': [42]}
-        elif self.kernel == 'poly':
+        elif self.__kernel == 'poly':
             grid_parameter = {'C' : C_range, 'degree' : d_range, 'coef0': r_range, 'random_state': [42]}
         else:
             grid_parameter = {'C' : C_range, 'random_state': [42]}
 
-        grid = GridSearchCV(SVC(kernel = self.kernel), param_grid = grid_parameter, cv = 3, scoring = 'accuracy', n_jobs = -1)
-        grid.fit(self.val_images, self.val_labels)
+        grid = GridSearchCV(SVC(kernel = self.__kernel), param_grid = grid_parameter, cv = 3, scoring = 'accuracy', n_jobs = -1)
+        grid.fit(self.__val_images, self.__val_labels)
 
         self.best_parameter = grid.best_params_
         self.best_score = grid.best_score_
 
     def __cross_validation(self):
         # Initiate a SVM model with the best parameter from gridsearch.
-        cv_model = SVC(kernel = self.kernel, **self.best_parameter)
+        cv_model = SVC(kernel = self.__kernel, **self.best_parameter)
 
         # Setup parameters for cross validation. 
         kf = KFold(n_splits = 5, shuffle = True, random_state = 42)
-        self.cv_score = cross_val_score(cv_model, self.train_images, self.train_labels, cv = kf)
+        self.cv_score = cross_val_score(cv_model, self.__train_images, self.__train_labels, cv = kf)
 
     def __prediction(self):
         # Setup a SVM model with the best kernel parameter from gridsearch.
-        clf = SVC(kernel = self.kernel, **self.best_parameter)
-        clf.fit(self.train_images, self.train_labels)
-        self.pred_labels = clf.predict(self.test_images)
+        clf = SVC(kernel = self.__kernel, **self.best_parameter)
+        clf.fit(self.__train_images, self.__train_labels)
+        self.__pred_labels = clf.predict(self.__test_images)
 
     def __evaluation(self):
         # Evaluate the performance of the model.
-        self.pred_score = metrics.accuracy_score(self.test_labels, self.pred_labels)
+        self.pred_score = metrics.accuracy_score(self.__test_labels, self.__pred_labels)
+
 
 def main():
-    linear_SVM = SVM('rbf', './Datasets/pneumoniamnist.npz')
-    best_parameter, best_score, cv_score = linear_SVM.train_model()
-    pred_score = linear_SVM.predict_model()
+    tree = TREE('./Datasets/pneumoniamnist.npz')
+    #best_parameter, best_score, cv_score = tree.train_model()
+    pred_score = tree.predict_model()
 
-    print('Best hyperparameter and its accuracy score: ', best_parameter, best_score)
-    print('Cross validation accuracy score: ', cv_score)
+    #print('Best hyperparameter and its accuracy score: ', best_parameter, best_score)
+    #print('Cross validation accuracy score: ', cv_score)
     print('Prediction accuracy score: ', pred_score)
 
     
 if __name__ == "__main__":
     main()
     
+
+class TREE():
+    def __init__(self, path):
+        # Load data from .npz file.
+        data = np.load(path)
+
+        train_images = data['train_images']
+        test_images = data['test_images']
+        val_images = data['val_images']
+        train_labels = data['train_labels']
+        test_labels = data['test_labels']
+        val_labels = data['val_labels']
+
+        # Reshaping the data to match the syntax of sklearn.
+        self.__train_labels = train_labels.ravel()
+        self.__test_labels = test_labels.ravel()
+        self.__val_labels = val_labels.ravel()
+        self.__train_images = train_images.reshape((train_images.shape[0], train_images[0].size))
+        self.__test_images = test_images.reshape((test_images.shape[0], test_images[0].size))
+        self.__val_images = val_images.reshape((val_images.shape[0], val_images[0].size))
+    
+    def predict_model(self):
+        self.__prediction()
+        self.__evaluation()
+        return self.pred_score
+
+    def __prediction(self):
+        # Setup a SVM model with the best kernel parameter from gridsearch.
+        clf = DecisionTreeClassifier()
+        clf.fit(self.__train_images, self.__train_labels)
+        self.__pred_labels = clf.predict(self.__test_images)
+        print(clf.get_depth(), clf.get_n_leaves(), clf.get_params())
+        # Default depth = 25, default leaves = 180
+    def __evaluation(self):
+        # Evaluate the performance of the model.
+        self.pred_score = metrics.accuracy_score(self.__test_labels, self.__pred_labels)
+
+
 
--- B/task_B.py
+++ B/task_B.py
@@ -1,3 +1,100 @@
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
-print(np.logspace(-3, 2, 6))
+class CustomImageDataset(Dataset):
+    def __init__(self, path, subset = 'train'):
+        data = np.load(path)
+
+        if subset == 'train':
+            self.images = torch.tensor(data['train_images'], dtype = torch.float32).permute(0, 3, 1, 2)
+            self.labels = torch.tensor(data['train_labels'], dtype = torch.long).squeeze(1)
+        elif subset == 'test':
+            self.images = torch.tensor(data['test_images'], dtype = torch.float32).permute(0, 3, 1, 2)
+            self.labels = torch.tensor(data['test_labels'], dtype = torch.long)
+        elif subset == 'val':
+            self.images = torch.tensor(data['val_images'], dtype = torch.float32).permute(0, 3, 1, 2)
+            self.labels = torch.tensor(data['val_labels'], dtype = torch.long).squeeze(1)
+        else:
+            raise ValueError("Use 'train', 'test' or 'val' to specify subsets")
+        
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        return self.images[index], self.labels[index]
+    
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size = 3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size = 3, stride=1, padding=1)
+        
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(64 * 3 * 3, 128)  # Adjusted for 28x28 input size
+        self.fc2 = nn.Linear(128, 9)  # Assuming 10 classes for classification
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+
+        x = x.view(-1, 64 * 3 * 3)  # Adjusted for 28x28 input size
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+train_images = CustomImageDataset('./Datasets/pathmnist.npz', 'train')
+test_images = CustomImageDataset('./Datasets/pathmnist.npz', 'test')
+val_images = CustomImageDataset('./Datasets/pathmnist.npz', 'val')
+
+dataloader = DataLoader(train_images, batch_size = 32, shuffle = True)
+
+model = CNN()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(dataloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in test_images:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = model(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
